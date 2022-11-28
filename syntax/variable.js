7446c3de6f73ad54e05d688410c1534d2842d64a
hscomet78 <hscomet78@gmail.com>
2022-11-29 04:36:49 +0900

    #

---
+++ syntax/variable.js
@@ -0,0 +1,13 @@
+var a = 1;
+console.log(a);
+var a = 2;
+console.log(a);
+
+var name = 'HSCOMET';
+var letter1 = 'Hello ' +name+ 
+' node.js!\n';
+console.log(letter1);
+
+var letter2 = `Hello ${name} 
+            node.js!`;
+console.log(letter2);
